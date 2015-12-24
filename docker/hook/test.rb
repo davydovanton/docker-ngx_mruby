4f226ab700b6950a1d7708ef588fca1b70b7da6e
Anton Davydov <antondavydov.o@gmail.com>
2015-12-24 17:16:28 +0300

    Add code for long requests

--- docker/hook/test.rb
+++ docker/hook/test.rb
@@ -1,1 +1,14 @@
-Nginx.echo "This is test for ngx_mruby"
+# docker stop $(docker ps -a -q)
+# sudo docker build -t local/docker-ngx_mruby  .
+# sudo docker run -p 80:80 -v /vagrant:/vagrant  -t local/dker-ngx_mruby
+#
+# check for 1k requests to server in one time
+
+r = Nginx::Request.new
+file_url = "/vagrant/#{r.uri.sub('/mruby-test/', '')}"
+
+until (File.exist?(file_url))
+  sleep 1
+end
+
+Nginx.echo "file exist : #{file_url} : #{File.exists?(file_url)}"
